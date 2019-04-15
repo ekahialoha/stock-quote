6f2176ff097946533da41eb2b4dd2ca18ba31ff8
Chris <chris@ekahi.co>
2019-04-15 12:01:52 -1000

    start of graph carousel, start of stock class

--- js/app.js
+++ js/app.js
@@ -1,26 +1,46 @@
+class Stock {
+    constructor (stockData) {
+        this.quote = stockData.quote;
+        this.logo = stockData.logo;
+        this.profile = stockData.company;
+        this.chart = { today: stockData.chart };
+    };
+
+    determinePositive () {
+        this.marketPositive = Math.sign(this.quote.change) > -1 ? true : false;
+    };
+
+    getPositive () {
+        return this.marketPositive;
+    }
+};
+
+const stocks = {};
+
 const runStockQuote = () => {
+    // Store current symbol
+    const $symbol = $('#symbol').val();
 
     const promise = $.ajax(
         {
-            url: `https://api.iextrading.com/1.0/stock/${$('#symbol').val()}/batch?types=quote,logo,company,chart&range=1d&chartInterval=10`
+            url: `https://api.iextrading.com/1.0/stock/${$symbol}/batch?types=quote,logo,company,chart&range=1d&chartInterval=10`
         }
     ).then(
         (data) => {
             // Store main container
+            stocks[$symbol] = new Stock(data);
             const $main = $('main').empty();
 
             console.log(data);
 
-            // Determine if value is positive or negative
-            const overallPositive = Math.sign(data.quote.change) > -1 ? true : false;
-
             // Main Section with merket graph
             const $mainSection = $('<section>').addClass('main-tab').appendTo($main);
-            $mainSection.append(`<h1>${data.company.symbol}</h1>`);
+            $mainSection.append(`<h1>${stocks[$symbol].profile.symbol}</h1>`);
             $mainSection.append(`<h3>${data.company.companyName}</h3>`);
-            $mainSection.append('<div class="graph-left-btn"></div>');
-            $mainSection.append('<canvas id="graph"></canvas>');
-            $mainSection.append('<div class="graph-right-btn"></div>');
+            const $graphContain = $('<div>').addClass('graph-container').appendTo($mainSection);
+            $graphContain.append('<div class="graph-btn material-icons">arrow_back</div>');
+            $graphContain.append('<div class="graph"><canvas id="graph"></canvas></div>');
+            $graphContain.append('<div class="graph-btn material-icons">arrow_forward</div>');
 
             // Stock Stats Section
             $subSection1 = $('<section>').appendTo($main);
@@ -31,8 +51,8 @@ const runStockQuote = () => {
             $dlStockStats.append(`<dt>52 Wk High</dt><dd>${data.quote.week52High}</dd>`);
             $dlStockStats.append(`<dt>52 Week Low</dt><dd>${data.quote.week52Low}</dd>`);
             $dlStockStats.append(`<dt>Change</dt><dd>${data.quote.change}</dd>`);
-            data.changePercent = (data.quote.changePercent *100).toFixed(2);
-            $dlStockStats.append(`<dt>Change %</dt><dd>${data.quote.changePercent}</dd>`);
+            data.changePercent = (data.quote.changePercent * 100).toFixed(2);
+            $dlStockStats.append(`<dt>Change %</dt><dd>${data.changePercent}</dd>`);
             $dlStockStats.append(`<dt>Sector</dt><dd>${data.quote.sector}</dd>`);
 
             // Stock Profile Section
@@ -65,7 +85,7 @@ const runStockQuote = () => {
             chartPlots.push(data.quote.close);
 
             // Chart Color
-            const chartColor = overallPositive === true ? '84, 138, 2' : '186, 56, 60';
+            const chartColor = stocks[$symbol].getPositive() === true ? '84, 138, 2' : '186, 56, 60';
 
             const graph = $('#graph');
             var dailyGraph = new Chart(graph, {
@@ -95,6 +115,5 @@ const runStockQuote = () => {
 };
 
 $(() => {
-    console.log(Math.round((new Date()).getTime() / 1000));
     $('form').on('submit', runStockQuote);
 });
