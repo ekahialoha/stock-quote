bade30b68e187c31bef4a50f2fc5d3113cad3fec
Chris <chris@ekahi.co>
2019-04-15 15:44:56 -1000

    graph carousel work, modify jQuery HTML and start of js click handlers

--- js/app.js
+++ js/app.js
@@ -37,31 +37,29 @@ const runStockQuote = () => {
             const $mainSection = $('<section>').addClass('main-tab').appendTo($main);
             $mainSection.append(`<h1>${stocks[$symbol].profile.symbol}</h1>`);
             $mainSection.append(`<h3>${data.company.companyName}</h3>`);
+
+            // Graphs
             const $graphContain = $('<div>').addClass('graph-container').appendTo($mainSection);
-            $graphContain.append('<div class="graph-btn material-icons">arrow_back</div>');
-            $graphContain.append('<div class="graph"><canvas id="graph"></canvas></div>');
-            $graphContain.append('<div class="graph-btn material-icons">arrow_forward</div>');
+            $prevGraph = $('<div>').addClass('graph-btn material-icons').text('arrow_back').appendTo($graphContain);
 
-            // Stock Stats Section
-            $subSection1 = $('<section>').appendTo($main);
-            $dlStockStats = $('<dl>').appendTo($subSection1);
-            $dlStockStats.append(`<dt>Last Updated</dt><dd>${data.quote.latestTime}</dd>`);
-            $dlStockStats.append(`<dt>Price</dt><dd>\$${data.quote.latestPrice}</dd>`);
-            $dlStockStats.append(`<dt>Previous Close</dt><dd>${data.quote.previousClose}</dd>`);
-            $dlStockStats.append(`<dt>52 Wk High</dt><dd>${data.quote.week52High}</dd>`);
-            $dlStockStats.append(`<dt>52 Week Low</dt><dd>${data.quote.week52Low}</dd>`);
-            $dlStockStats.append(`<dt>Change</dt><dd>${data.quote.change}</dd>`);
-            data.changePercent = (data.quote.changePercent * 100).toFixed(2);
-            $dlStockStats.append(`<dt>Change %</dt><dd>${data.changePercent}</dd>`);
-            $dlStockStats.append(`<dt>Sector</dt><dd>${data.quote.sector}</dd>`);
+            const graphs = [
+                { name: 'Daily', class: 'daily-graph' },
+                { name: 'Week', class: 'week-graph' },
+                { name: 'Month', class: 'mon-graph'}
+            ];
 
-            // Stock Profile Section
-            $subSection2 = $('<section>').appendTo($main);
-            $dlStockProfile = $('<dl>').appendTo($subSection2);
-            $dlStockProfile.append(`<dt>Sector</dt><dd>${data.company.sector}</dd>`);
-            $dlStockProfile.append(`<dt>Industry</dt><dd>${data.company.industry}</dd>`);
-            $dlStockProfile.append(`<dt>Website</dt><dd>${data.company.website}</dd>`);
-            $dlStockProfile.append(`<dt>Description</dt><dd>${data.company.description}</dd>`);
+            $graphContain.append('<div class="graph"><canvas id="daily-graph"></canvas><canvas id="week-graph" style="display: none;"></canvas></div>');
+            $nextGraph = $('<div>').addClass('graph-btn material-icons').text('arrow_forward').appendTo($graphContain);
+
+            // Graph carousel click handlers
+            let currentGraph = 0;
+            $prevGraph.on('click', () => {
+                console.log('prev');
+            });
+
+            $nextGraph.on('click', () => {
+                console.log('next');
+            });
 
             // Chart Plots Arrays
             let chartLabels = [];
@@ -87,13 +85,12 @@ const runStockQuote = () => {
             // Chart Color
             const chartColor = stocks[$symbol].getPositive() === true ? '84, 138, 2' : '186, 56, 60';
 
-            const graph = $('#graph');
-            var dailyGraph = new Chart(graph, {
+            const dailyGraph = new Chart($('#daily-graph'), {
                 type: 'line',
                 data: {
                     labels: chartLabels,
                     datasets: [{
-                        label: data.company.symbol,
+                        label: 'Daily',
                         data: chartPlots,
                         backgroundColor: [
                             `rgba(${chartColor}, 0.5)`
@@ -105,6 +102,44 @@ const runStockQuote = () => {
                     }]
                 },
             });
+            const weekGraph = new Chart($('#week-graph'), {
+                type: 'line',
+                data: {
+                    labels: chartLabels,
+                    datasets: [{
+                        label: 'Week',
+                        data: chartPlots,
+                        backgroundColor: [
+                            `rgba(${chartColor}, 0.5)`
+                        ],
+                        borderColor: [
+                            `rgb(${chartColor})`
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+            });
+            // Stock Stats Section
+            $subSection1 = $('<section>').appendTo($main);
+            $dlStockStats = $('<dl>').appendTo($subSection1);
+            $dlStockStats.append(`<dt>Last Updated</dt><dd>${data.quote.latestTime}</dd>`);
+            $dlStockStats.append(`<dt>Price</dt><dd>\$${data.quote.latestPrice}</dd>`);
+            $dlStockStats.append(`<dt>Previous Close</dt><dd>${data.quote.previousClose}</dd>`);
+            $dlStockStats.append(`<dt>52 Wk High</dt><dd>${data.quote.week52High}</dd>`);
+            $dlStockStats.append(`<dt>52 Week Low</dt><dd>${data.quote.week52Low}</dd>`);
+            $dlStockStats.append(`<dt>Change</dt><dd>${data.quote.change}</dd>`);
+            data.changePercent = (data.quote.changePercent * 100).toFixed(2);
+            $dlStockStats.append(`<dt>Change %</dt><dd>${data.changePercent}</dd>`);
+            $dlStockStats.append(`<dt>Sector</dt><dd>${data.quote.sector}</dd>`);
+
+            // Stock Profile Section
+            $subSection2 = $('<section>').appendTo($main);
+            $dlStockProfile = $('<dl>').appendTo($subSection2);
+            $dlStockProfile.append(`<dt>Sector</dt><dd>${data.company.sector}</dd>`);
+            $dlStockProfile.append(`<dt>Industry</dt><dd>${data.company.industry}</dd>`);
+            $dlStockProfile.append(`<dt>Website</dt><dd>${data.company.website}</dd>`);
+            $dlStockProfile.append(`<dt>Description</dt><dd>${data.company.description}</dd>`);
+
         },
         (err) => {
             console.log(err);
