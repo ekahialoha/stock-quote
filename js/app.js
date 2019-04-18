22e2f3e4f587e49927b996a90bca43e21f0f3e77
Chris <chris@ekahi.co>
2019-04-17 20:39:11 -1000

    making use of class

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+// ==============================
+// Stock Class, data passed from
+// API calls to be parsed and
+// manipulated as necessary to
+// then display within the DOM.
+// ==============================
+
 class Stock {
     constructor (stockData) {
         this.quote = stockData.quote;
@@ -7,14 +14,76 @@ class Stock {
         this.determinePositive ();
     };
 
+    // Accepts data from API and stores for our uses
+    setGraphData (graphData, graphLabel = 'day') {
+        this.chart[graphLabel] = graphData;
+    };
+
+    getLogo () {
+        return this.logo.url;
+    };
+
+    getStockName () {
+        return this.profile.companyName;
+    };
+
+    getSymbol () {
+        return this.profile.symbol;
+    };
+
+    getLastUpdate () {
+        return this.quote.latestTime;
+    };
+
+    getLatestPrice () {
+        return this.quote.latestPrice;
+    };
+
+    getIsPositive () {
+        return Math.sign(this.quote.change) > -1 ? true : false;
+    };
+
+    getChange () {
+        return this.quote.change;
+    };
+
+    getPercentChange () {
+        return (this.quote.changePercent * 100).toFixed(2);
+    };
+
+    get52WkHigh () {
+        return this.quote.week52High;
+    };
+
+    get52WkLow () {
+        return this.week52Low;
+    };
+
+    getSector () {
+        return this.profile.sector;
+    };
+
+    getIndustry () {
+        return this.profile.industry;
+    };
+
+    getWebsite () {
+        return this.profile.website;
+    }
+
+    getDescription () {
+        return this.profile.description;
+    }
+
+    getChartData (whichChart) {
+        return this.chart[whichChart];
+    };
+
     determinePositive () {
         this.marketPositive = Math.sign(this.quote.change) > -1 ? true : false;
     };
 
-    getPositive () {
-        return this.marketPositive;
-    }
-};
+}
 
 const stocks = {};
 
@@ -33,29 +102,28 @@ const runStockQuote = () => {
             // Store main container
             stocks[$symbol] = new Stock(data);
 
+            // Create reference to stock class
+            const currStock = stocks[$symbol];
+
             // Ensure working with an empty canvas
             const $main = $('main').empty();
 
-            console.log(data);
-
-            let chartData = null;
-
+            // Grab other graph data
             const chartPromise = $.ajax(
                 { url: `https://api.iextrading.com/1.0/stock/${$symbol}/chart/1m` }
             ).then((apiData) => {
-                stocks[$symbol].chart.month = apiData;
-                stocks[$symbol].chart.week = apiData.slice(apiData.length - 7);
+                currStock.setGraphData(apiData, 'month');
+                currStock.setGraphData(apiData.slice(apiData.length - 7), 'week')
             });
 
             // Main Section with market graph
             const $mainSection = $('<section>').addClass('main-tab').appendTo($main);
-            $mainSection.append(`<div class="logo"><img src="${data.logo.url}" alt="${data.company.companyName}" /></div>`)
-            $mainSection.append(`<h1>${stocks[$symbol].profile.symbol}</h1>`);
-            $mainSection.append(`<h3>${data.company.companyName}</h3>`);
+            $mainSection.append(`<div class="logo"><img src="${currStock.getLogo()}" alt="${currStock.getStockName()}" /></div>`)
+            $mainSection.append(`<h1>${currStock.getSymbol()}</h1>`);
+            $mainSection.append(`<h3>${currStock.getStockName()}</h3>`);
 
             // Graph Container
             const $graphContain = $('<div>').addClass('graph-container').appendTo($mainSection);
-
 
             // Build Graphs: Day, Week, Month
             let chartJs = null;
@@ -65,28 +133,28 @@ const runStockQuote = () => {
                 let chartLabels = [];
                 let chartPlots = [];
 
-                let data = null;
-                let currentChartName = null;
+                // Create variables
+                let chartData = null;
+                let chartName = null;
                 let chartColor = null;
 
                 switch (type) {
                     case 'week':
-                        currentChartName = 'Week';
-                        data = stocks[$symbol].chart.week;
+                        chartName = 'Week';
+                        chartData = currStock.getChartData('week');
                     break;
 
                     case 'month':
-                        currentChartName = 'Month';
-                        data = stocks[$symbol].chart.month;
+                        chartName = 'Month';
+                        chartData = currStock.getChartData('month');
                     break;
 
                     case 'day':
                     default:
-                        currentChartName = 'Day';
-                        data = stocks[$symbol].chart.day;
+                        chartName = 'Day';
+                        chartData = currStock.getChartData('day');
                     break;
                 }
-                console.log(data);
 
                 if (type === 'day') {
                     // Push Opening Plot
@@ -95,7 +163,7 @@ const runStockQuote = () => {
                 }
 
                 // Chart individual plots
-                data.forEach((plot) => {
+                chartData.forEach((plot) => {
                     if (type !== 'day') {
                         // Chart individual plots
                         chartLabels.push(plot.date);
@@ -103,8 +171,6 @@ const runStockQuote = () => {
 
                         // Chart Color
                         const positive = Math.sign(chartPlots[0] - chartPlots[chartPlots.length-1]) > -1 ? false : true;
-                        console.log(chartPlots[0] - chartPlots[chartPlots.length-1]);
-                        console.log(Math.sign(chartPlots[0] - chartPlots[chartPlots.length-1]));
                         chartColor = positive === true ? '84, 138, 2' : '186, 56, 60';
                     } else {
                         // Skip over data without any useful information
@@ -113,7 +179,7 @@ const runStockQuote = () => {
                             chartLabels.push(plot.label);
                             chartPlots.push(segmentClose);
                         }
-                        chartColor = stocks[$symbol].getPositive() === true ? '84, 138, 2' : '186, 56, 60';
+                        chartColor = stocks[$symbol].getIsPositive() === true ? '84, 138, 2' : '186, 56, 60';
                     }
                 });
 
@@ -134,133 +200,17 @@ const runStockQuote = () => {
                     data: {
                         labels: chartLabels,
                         datasets: [{
-                            label: currentChartName,
+                            label: chartName,
                             data: chartPlots,
-                            backgroundColor: `rgba(${chartColor}, 0.8)`,
+                            backgroundColor: `rgba(${chartColor}, 0.6)`,
                             borderColor: `rgb(${chartColor})`,
-                            borderWidth: 1,
+                            borderWidth: 4,
                         }]
                     },
                 });
             };
 
-            const dailyGraph = () => {
-
-                // Chart Plots Arrays
-                let chartLabels = [];
-                let chartPlots = [];
-
-                // Push Opening Plot
-                chartLabels.push('Open');
-                chartPlots.push(data.quote.open);
-
-                // Chart individual plots
-                data.chart.forEach((plot) => {
-                    // Skip over data without any useful information
-                    const segmentClose = (plot.marketClose === null) ? plot.close : plot.marketClose;
-                    if (segmentClose !== null) {
-                        chartLabels.push(plot.label);
-                        chartPlots.push(segmentClose);
-                    }
-                });
-
-                // Push Closing/Current Plot
-                if (data.quote.latestSource === 'Close' || data.quote.latestSource === 'Previous close') {
-                    chartLabels.push('Close');
-
-                } else {
-                    chartLabels.push('Most Recent');
-                }
-                chartPlots.push(data.quote.latestPrice);
-
-                // Chart Color
-                const chartColor = stocks[$symbol].getPositive() === true ? '84, 138, 2' : '186, 56, 60';
-                chartJs = new Chart($('#graph'), {
-                    type: 'line',
-                    data: {
-                        labels: chartLabels,
-                        datasets: [{
-                            label: 'Daily',
-                            data: chartPlots,
-                            backgroundColor: `rgba(${chartColor}, 0.8)`,
-                            borderColor: `rgb(${chartColor})`,
-                            borderWidth: 1,
-                        }]
-                    },
-                });
-            };
-            const weekGraph = () => {
-                console.log(stocks[$symbol].chart);
-                // Chart Plots Arrays
-                let chartLabels = [];
-                let chartPlots = [];
-
-                // Chart individual plots
-                stocks[$symbol].chart.week.forEach((plot) => {
-                    chartLabels.push(plot.date);
-                    chartPlots.push(plot.close);
-                });
-
-                // Chart Color
-                const positive = Math.sign(chartPlots[0] - chartPlots[chartPlots.length-1]) > -1 ? false : true;
-                console.log(chartPlots[0] - chartPlots[chartPlots.length-1]);
-                console.log(Math.sign(chartPlots[0] - chartPlots[chartPlots.length-1]));
-                const chartColor = positive === true ? '84, 138, 2' : '186, 56, 60';
-                chartJs = new Chart($('#graph'), {
-                    type: 'line',
-                    data: {
-                        labels: chartLabels,
-                        datasets: [{
-                            label: 'Week',
-                            data: chartPlots,
-                            backgroundColor: [
-                                `rgba(${chartColor}, 0.5)`
-                            ],
-                            borderColor: [
-                                `rgb(${chartColor})`
-                            ],
-                            borderWidth: 1
-                        }]
-                    }
-                });
-            };
-            const monthGraph = () => {
-                console.log(stocks[$symbol].chart);
-                // Chart Plots Arrays
-                let chartLabels = [];
-                let chartPlots = [];
-
-                // Chart individual plots
-                stocks[$symbol].chart.month.forEach((plot) => {
-                    chartLabels.push(plot.date);
-                    chartPlots.push(plot.close);
-                });
-
-                // Chart Color
-                const positive = Math.sign(chartPlots[0] - chartPlots[chartPlots.length-1]) > -1 ? false : true;
-                console.log(chartPlots[0] - chartPlots[chartPlots.length-1]);
-                console.log(Math.sign(chartPlots[0] - chartPlots[chartPlots.length-1]));
-                const chartColor = positive === true ? '84, 138, 2' : '186, 56, 60';
-                chartJs = new Chart($('#graph'), {
-                    type: 'line',
-                    data: {
-                        labels: chartLabels,
-                        datasets: [{
-                            label: 'Month',
-                            data: chartPlots,
-                            backgroundColor: [
-                                `rgba(${chartColor}, 0.5)`
-                            ],
-                            borderColor: [
-                                `rgb(${chartColor})`
-                            ],
-                            borderWidth: 1
-                        }]
-                    }
-                });
-            };
-
-            // Register functions for carousel
+            // Register graph options for carousel
             const graphs = ['day', 'week', 'month'];
 
             // Previous graph button
@@ -303,23 +253,22 @@ const runStockQuote = () => {
             $asideColumn = $('<aside>').appendTo($main);
             $subSection1 = $('<section>').appendTo($asideColumn);
             $dlStockStats = $('<dl>').appendTo($subSection1);
-            $dlStockStats.append(`<dt>Last Updated</dt><dd>${data.quote.latestTime}</dd>`);
-            $dlStockStats.append(`<dt>Price</dt><dd>\$${data.quote.latestPrice}</dd>`);
+            $dlStockStats.append(`<dt>Last Updated</dt><dd>${currStock.getLastUpdate()}</dd>`);
+            $dlStockStats.append(`<dt>Price</dt><dd>\$${currStock.getLatestPrice()}</dd>`);
             $dlStockStats.append(`<dt>Previous Close</dt><dd>${data.quote.previousClose}</dd>`);
-            $dlStockStats.append(`<dt>52 Wk High</dt><dd>${data.quote.week52High}</dd>`);
-            $dlStockStats.append(`<dt>52 Week Low</dt><dd>${data.quote.week52Low}</dd>`);
-            const chngStyle = stocks[$symbol].getPositive() === true ? 'positive' : 'negative';
-            $dlStockStats.append(`<dt>Change</dt><dd class="${chngStyle}">${data.quote.change}</dd>`);
+            const chngStyle = currStock.getIsPositive() === true ? 'positive' : 'negative';
             data.changePercent = (data.quote.changePercent * 100).toFixed(2);
-            $dlStockStats.append(`<dt>Change %</dt><dd class="${chngStyle}">${data.changePercent}</dd>`);
+            $dlStockStats.append(`<dt>Change</dt><dd><span class="${chngStyle}">${currStock.getChange()}</span> (<span class="${chngStyle}">${currStock.getPercentChange()}%</span>)</dd>`);
+            $dlStockStats.append(`<dt>52 Wk High</dt><dd>${currStock.get52WkHigh()}</dd>`);
+            $dlStockStats.append(`<dt>52 Week Low</dt><dd>${currStock.get52WkLow()}</dd>`);
 
             // Stock Profile Section
             $subSection2 = $('<section>').appendTo($asideColumn);
             $dlStockProfile = $('<dl>').appendTo($subSection2);
-            $dlStockProfile.append(`<dt>Sector</dt><dd>${data.company.sector}</dd>`);
-            $dlStockProfile.append(`<dt>Industry</dt><dd>${data.company.industry}</dd>`);
-            $dlStockProfile.append(`<dt>Website</dt><dd>${data.company.website}</dd>`);
-            $subSection2.append(`<div>${data.company.description}</div>`);
+            $dlStockProfile.append(`<dt>Sector</dt><dd>${currStock.getSector()}</dd>`);
+            $dlStockProfile.append(`<dt>Industry</dt><dd>${currStock.getIndustry()}</dd>`);
+            $dlStockProfile.append(`<dt>Website</dt><dd>${currStock.getWebsite()}</dd>`);
+            $subSection2.append(`<div>${currStock.getDescription()}</div>`);
 
         },
         (err) => {
@@ -331,6 +280,7 @@ const runStockQuote = () => {
 };
 
 $(() => {
+
     $('form').on('submit', runStockQuote);
     // console.log($($.parseXML('<test>Testing</test>')).text());
 });
